82a73a3817ad5aa8fcaf9a2f1baf82d522549039
jmrzy <mrzygja@proton.me>
2024-10-30 00:17:05 +0000

    Added custom attribute with pokemon name

---
+++ Jmrz/PokemonApi/Setup/InstallData.php
@@ -0,0 +1,49 @@
+<?php
+namespace Jmrz\PokemonApi\Setup;
+
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+
+class InstallData implements InstallDataInterface
+{
+    private $eavSetupFactory;
+
+    public function __construct(EavSetupFactory $eavSetupFactory)
+    {
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $setup->startSetup();
+
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+        $eavSetup->addAttribute(
+            \Magento\Catalog\Model\Product::ENTITY,
+            'pokemon_name',
+            [
+                'group' => 'General',
+                'attribute_set_id' => 'Default',    
+                'type' => 'varchar',
+                'label' => 'Pokemon Name',
+                'input' => 'text',
+                'global' => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_GLOBAL,
+                'visible' => true,
+                'required' => false,
+                'user_defined' => true,
+                'position' => 30,
+                'searchable' => true,
+                'filterable' => true,
+                'comparable' => false,
+                'visible_on_front' => false,
+                'used_in_product_listing' => true,
+                'unique' => false,
+                'system' => 0
+            ]
+        );
+
+        $setup->endSetup();
+    }
+}
